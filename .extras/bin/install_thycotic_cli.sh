ba89e805ea2d257d65a459a9de53b23c33be1096
Adrian Juhl <adrian.juhl@gmail.com>
2025-12-14 19:51:20 +1030

    WIP

--- .extras/bin/install_thycotic_cli.sh
+++ .extras/bin/install_thycotic_cli.sh
@@ -61,7 +61,7 @@ install_thycotic_cli()
     abort_script
   fi
 
-  ANSIBLE_VERBOSE_ARGUMENT="-vvv"
+  ANSIBLE_VERBOSE_ARGUMENT="-v"
 
   ASK_BECOME_PASS_OPTION=""
   if [ "${REQUIRES_BECOME}" = "${TRUE_STRING}" ]; then
